696136711f7ffbac33d5a4f181daaf7b411bf2ee
comseung18 <comseung18@koreatech.ac.kr>
2021-04-07 10:36:44 +0900

    IE class Create Secet class Create

--- IEAuto/IE.cs
+++ IEAuto/IE.cs
@@ -8,6 +8,62 @@ namespace IEAuto
 {
     class IE
     {
-
+        SHDocVw.InternetExplorer ie;
+        public MSHTML.HTMLDocument Document
+        {
+            get
+            {
+                return ie.Document;
+            }
+        }
+        public string LastErrorMsg { get; set; }
+        public IE()
+        {
+            try
+            {
+                ie = new SHDocVw.InternetExplorer();
+            }
+            catch (Exception e)
+            {
+                LastErrorMsg = e.Message;
+            }
+        }
+        void Wait()
+        {
+            while(ie.Busy || ie.ReadyState != SHDocVw.tagREADYSTATE.READYSTATE_COMPLETE)
+            {
+                System.Threading.Thread.Sleep(100);
+            }
+        }
+        public int Browse(string url)
+        {
+            object ourl = url;
+            try
+            {
+                ie.Navigate2(ref ourl);
+                Wait();
+            }
+            catch ( Exception e )
+            {
+                LastErrorMsg = e.Message;
+                return -1;
+            }
+            return 0;
+        }
+        public int Close()
+        {
+            try
+            {
+                ie.Quit();
+            }
+            catch ( Exception e)
+            {
+                LastErrorMsg = e.Message;
+                return -1;
+            }
+            ie = null;
+            return 0;
+        }
+        
     }
 }
---
+++ IEAuto/Secret.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace IEAuto
+{
+    class Secret
+    {
+        SHA256 sha;
+        public Secret()
+        {
+            sha = new SHA256Managed();
+        }
+
+        public string SHA256Hash(string data)
+        {
+            byte[] hash = sha.ComputeHash(Encoding.ASCII.GetBytes(data));
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach(byte b in hash)
+            {
+                stringBuilder.AppendFormat("{0:x2}", b);
+            }
+            return stringBuilder.ToString();
+        }
+
+        // 암호화 키 생성
+        public static Rfc2898DeriveBytes CreateKey(string passwd)
+        {
+            byte[] keyBytes = Encoding.UTF8.GetBytes(passwd);
+            byte[] saltBytes = SHA512.Create().ComputeHash(keyBytes);
+
+            Rfc2898DeriveBytes result = new Rfc2898DeriveBytes(keyBytes, saltBytes, 100000);
+            return result;
+        }
+
+        
+        public static byte[] AesEncrypt(byte[] origin, string passwd)
+        {
+            RijndaelManaged aes = new RijndaelManaged();
+            Rfc2898DeriveBytes key = CreateKey(passwd);
+            Rfc2898DeriveBytes vector = CreateKey(passwd);
+
+            aes.BlockSize = 128;
+            aes.KeySize = 256;
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+            aes.Key = key.GetBytes(32);
+            aes.IV = vector.GetBytes(16);
+
+            ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+            using(MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                {
+                    cs.Write(origin, 0, origin.Length);
+                }
+                return ms.ToArray();
+            }
+        }
+
+        public static byte[] AesDecrypt(byte[] origin, string password)
+        {
+            RijndaelManaged aes = new RijndaelManaged();   
+            Rfc2898DeriveBytes key = CreateKey(password);    
+            Rfc2898DeriveBytes vector = CreateKey(password);   
+
+            aes.BlockSize = 128;          
+            aes.KeySize = 256;           
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+            aes.Key = key.GetBytes(32);   
+            aes.IV = vector.GetBytes(16);  
+
+            ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+            using (MemoryStream ms = new MemoryStream()) 
+            {
+                
+                using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Write))
+                {
+                    cs.Write(origin, 0, origin.Length);
+                }
+                return ms.ToArray();
+            }
+        }
+    }
+}
